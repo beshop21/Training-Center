4a4d0f1f25e415d47d41ffc9c5ebd0c45dd62b70
mario <cabdicasiisismail51@gmail.com>
2026-06-16 10:37:58 +0300

    Add Payment Data

---
+++ DataLayer/PaymentsData.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class PaymentsData
+    {
+
+        public static int InsertNewPayment(string StudentName, string CourseName, DateTime PaymanetData, float AmountOFpaid, string PaymentMethod)
+        {
+            int CourseId = -1;
+
+            SqlConnection connection = new SqlConnection(DataSettings.Stringconnection);
+
+            string query = @"insert into Courses (Title,Hourse,Price,StartDate,IsActive) 
+                     VALUES (@Title,@Hourse,@Price,@StartDate,@IsActive);
+                     SELECT SCOPE_IDENTITY();";
+
+            SqlCommand comnd = new SqlCommand(query, connection);
+
+            comnd.Parameters.AddWithValue("@Title", StudentName);
+            comnd.Parameters.AddWithValue("@Hourse", CourseName);
+            comnd.Parameters.AddWithValue("@Price", PaymanetData);
+            comnd.Parameters.AddWithValue("@StartDate", AmountOFpaid);
+            comnd.Parameters.AddWithValue("@IsActive", PaymentMethod);
+
+            try
+            {
+                connection.Open();
+
+                object result = comnd.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int ID))
+                {
+                    CourseId = ID;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return CourseId;
+        }
+
+        public static bool UpdateCourse(int id, string title, int hours, float price, DateTime startdate, bool isactive)
+        {
+            int Effectnumber = -1;
+
+            SqlConnection connection = new SqlConnection(DataSettings.Stringconnection);
+
+            string qeury = @"update Courses set 
+                          Title=@Title,
+                          Hourse=@Hourse,
+                          Price=@Price,
+                          StartDate=@StartDate,
+                          IsActive=@IsActive where CourseID=@CourseID";
+
+            SqlCommand comnd = new SqlCommand(qeury, connection);
+
+            comnd.Parameters.AddWithValue("@CourseID", id);
+            comnd.Parameters.AddWithValue("@Title", title);
+            comnd.Parameters.AddWithValue("@Hourse", hours);
+            comnd.Parameters.AddWithValue("@Price", price);
+            comnd.Parameters.AddWithValue("@StartDate", startdate);
+
+            comnd.Parameters.AddWithValue("@IsActive", isactive);
+
+            try
+            {
+                connection.Open();
+
+                Effectnumber = comnd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return (Effectnumber > 0);
+        }
+
+
+        public static bool DeleteCourse(int id)
+        {
+            int effectnumber = -1;
+            SqlConnection connection = new SqlConnection(DataSettings.Stringconnection);
+
+            string query = "delete from Courses where CourseID=@CourseID";
+            SqlCommand commd = new SqlCommand(query, connection);
+            commd.Parameters.AddWithValue("@CourseID", id);
+            try
+            {
+                connection.Open();
+                effectnumber = commd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return (effectnumber > 0);
+        }
+
+
+        public static bool GetCourseByID(int id, ref string Title, ref int Hourse,
+    ref float Price, ref DateTime StartDate, ref bool IsActive)
+        {
+            bool isfound = false;
+
+            SqlConnection connection = new SqlConnection(DataSettings.Stringconnection);
+
+            string qeury = "select * from Courses where CourseID=@CourseID";
+
+            SqlCommand commd = new SqlCommand(qeury, connection);
+
+            commd.Parameters.AddWithValue("@CourseID", id);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = commd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isfound = true;
+                    Title = (string)reader["Title"];
+                    Hourse = (int)reader["Hourse"];
+                    Price = Convert.ToSingle(reader["Price"]);
+                    StartDate = (DateTime)reader["StartDate"];
+                    IsActive = (bool)reader["IsActive"];
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isfound;
+        }
+
+        public static bool GetCourseByName(ref int id, string Title, ref int Hourse,
+  ref float Price, ref DateTime StartDate, ref bool IsActive)
+        {
+            bool isfound = false;
+
+            SqlConnection connection = new SqlConnection(DataSettings.Stringconnection);
+
+            string qeury = "select * from Courses where Title=@Title";
+
+            SqlCommand commd = new SqlCommand(qeury, connection);
+
+            commd.Parameters.AddWithValue("@Title", Title);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = commd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isfound = true;
+                    id = (int)reader["CourseID"];
+                    Hourse = (int)reader["Hourse"];
+                    Price = Convert.ToSingle(reader["Price"]);
+                    StartDate = (DateTime)reader["StartDate"];
+                    IsActive = (bool)reader["IsActive"];
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isfound;
+        }
+
+        public static DataTable GetAllRecode()
+        {
+            DataTable tb = new DataTable();
+            SqlConnection connection = new SqlConnection(DataSettings.Stringconnection);
+            string query = "select * from Courses";
+            SqlCommand command = new SqlCommand(query, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    tb.Load(reader);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+            return tb;
+        }
+
+
+    }
+}
